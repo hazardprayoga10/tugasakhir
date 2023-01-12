00929b343aff275f7f36950ec82deeafa519f5e6
hazardprayoga10 <120264001+hazardprayoga10@users.noreply.github.com>
2023-01-12 20:30:27 +0700

    Add files via upload

---
+++ undangan2/script.js
@@ -0,0 +1,8 @@
+var myVideo = document.getElementById("video1"); 
+        function play() { 
+            myVideo.play();
+        }
+        function pause() {
+            myVideo.pause();
+        }
+    
